e33fbef675e609189861d062b4270bddf48cfa37
kimisaraz <kimisaraz@gmail.com>
2018-10-05 15:16:05 +0900

    Add test runner

---
+++ run.rb
@@ -0,0 +1,25 @@
+require 'minruby'
+require 'colorize'
+require 'pp'
+require 'stringio'
+
+MY_PROGRAM = 'interp.rb'
+
+Dir.glob('test*.rb').sort.each do |f|
+  correct = `ruby -I. -rfizzbuzz #{f}`
+  answer = `ruby #{MY_PROGRAM} #{f}`
+
+  print "#{f} => "
+  if correct == answer
+    puts "OK!".green
+  else
+    puts "NG".red
+
+    out = StringIO.new
+    PP.pp(minruby_parse(File.read(f)), out)
+    out.rewind
+    puts out.read.yellow
+
+    exit(1)
+  end
+end
